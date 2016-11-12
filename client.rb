35bbb35d24a7fc2bf09c158c58816398ba2cbd16
Abdul7 <razzaq.abdul1467@gmail.com>
2016-11-11 20:10:11 -0800

    file

--- client.rb
+++
@@ -1,8 +0,0 @@
-log_level        :info
-log_location     STDOUT
-chef_server_url  'https://api.chef.io/organizations/abdul'
-validation_client_name 'abdul-validator'
-validation_key '/etc/chef/abdul-validator.pem'
-client_key '/etc/chef/client.pem'
-ssl_verify_mode    :verify_none
-                             
